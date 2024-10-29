4c2b5714126e7cbedcb9c9c52ec7c8b4b109cc33
SalomoneCro <pedrosalomone@hotmail.com>
2024-10-29 15:42:31 -0300

    POST method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 from fastapi.responses import JSONResponse
 
@@ -9,11 +9,13 @@ movies = [
     {
         "id": 1,
         "name": "The Big Short",
+        "category" : "Documentary",
         "description": "Some random guys predict one of the biggest economic crashes of all time"
     },
     {
         "id": 2,
         "name": "American Pie",
+        "category": "Comedy",
         "description": "Frat parties, alcohol, sex, all a man can ask"
     }
 ]
@@ -32,3 +34,24 @@ def get_movie(id: int):
         if movie["id"] == id:
             return movie
     return "No se encontro la pelicula"
+
+@app.get("/movies/", tags=["movies"])
+def get_movies_category(category: str):
+    return [i for i in movies if i["category"] == category]
+
+@app.post("/movies/add", tags=['movies'])
+def add_movie(id: int = Body(),
+              name: str = Body(),
+              category: str = Body(),
+              description: str = Body()
+):
+    movies.append({
+        "id": id,
+        "name": name,
+        "category": category,
+        "description": description,
+    })
+    return movies
+
+app.
+    
